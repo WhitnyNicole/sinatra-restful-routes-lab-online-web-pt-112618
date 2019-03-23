e3d41ad25124c04bdee2ef2770b1e626a031269b
Whitny Edwards <wne4e2@gmail.com>
2019-03-23 21:19:27 +0000

    Automatically backed up by Learn

--- db/migrate/20190323211856_recipes.rb
+++ db/migrate/20190323211856_recipes.rb
@@ -1,4 +1,14 @@
 class Recipes < ActiveRecord::Migration
-  def change
+  class CreateRecipes < ActiveRecord::Migration
+  def up
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.string :cook_time
+    end
+  end
+
+  def down
+    drop_table :recipes
   end
 end
